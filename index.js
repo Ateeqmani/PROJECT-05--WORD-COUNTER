36423736248e4c66788bde46dde75b2cfdc93d1f
Ateeqmani <maniwrites007@gmail.com>
2023-11-07 23:28:22 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("Welcome To The Word Counter :)");
+const counter = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Write SomeThing To Count Yours Words. "
+    }
+]);
+const w = counter.sentence.trim().split("");
+console.log(`Your Lenght Of The Words : ${w.length}`);
